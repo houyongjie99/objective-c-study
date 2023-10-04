c675d6c4d22ac6e33a359cd8a5134f64416e10f8
hyj_Gtihub <17638935643@163.com>
2023-10-04 12:58:29 +0800

    add

--- 08-OC类方法/08-OC类方法.m
+++ 08-OC类方法/08-OC类方法.m
@@ -4,12 +4,29 @@
 //Created by hyj on 2023/10/2
 //
 /*
-
+语法：
+    和对象方法类似，只需要将 - 改成 +即可
 */
 
 #import <Foundation/Foundation.h>
 
-int main() {
+@interface Person : NSObject
+{
+    int _age;
+}
+- (void)run;
++ (void)walk;
+@end
 
+@implementaion Person
+- (void)run {
+    NSLog(@"我是对象方法");
+}
++ (void)walk {
+    NSLog(@"我是类方法");
+}
+@end
+int main() {
+    [Person walk];
     return 0;
 }
